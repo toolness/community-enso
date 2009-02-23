5debad640cdd92e67d3b69fdb507f889c37f9eee
stuart.langridge@canonical.com <>
2009-02-23 00:52:15 +0000

    Add win32 implementation of get_script_folder_name as per blackdaemon mailing list post

--- enso/platform/win32/scriptfolder.py
+++ enso/platform/win32/scriptfolder.py
@@ -1,9 +1,18 @@
+from win32com.shell import shell, shellcon
+
+SYSTEMFOLDER_APPDATALOCAL = shellcon.CSIDL_LOCAL_APPDATA
+
+def get_system_folder(folder_id):
+   return shell.SHGetFolderPath(0, folder_id, 0, 0)
 
 def get_script_folder_name():
   """Returns the folder where Enso commands are found. This function
      is responsible for ensuring that this folder exists: it must not
      return a path that is not present! It is expected to place this
      folder in some platform-specific logical location."""
-  raise NotImplementedError("This platform does not define a "
-      "scripts folder (this needs fixing)")
+  SPECIALFOLDER_ENSOLOCAL_COMMANDS = os.path.join(
+   get_system_folder(SYSTEMFOLDER_APPDATALOCAL), "Enso", "commands")
+  if not os.path.isdir(SPECIALFOLDER_ENSOLOCAL_COMMANDS):
+    os.makedirs(SPECIALFOLDER_ENSOLOCAL_COMMANDS)
+  return SPECIALFOLDER_ENSOLOCAL_COMMANDS
 
